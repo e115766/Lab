e2b3f14dd8a3f4a0348e88c86fd879edcde3e546
Tasuku KINJO <e115766@ie.u-ryukyu.ac.jp>
2014-06-28 20:48:47 +0900

    saturday

--- test.rb
+++ test.rb
@@ -1,5 +1,15 @@
 require 'mysql'
-db = Mysql.connect('localhost', 'root', '', 'diseases')
-db.query('select id, name, diagnose from diseases where diagnose like "%症状%"').each do |col1, col2, col3|
-    print col3.gsub(/<\/?[^>]*>/, "")
+require 'MeCab'
+#db = Mysql.connect('localhost', 'root', '', 'diseases')
+#db.query('select id, name, diagnose from diseases where diagnose like "%症状の現れ方%"').each do |col1, col2, col3|
+#    print col3.gsub(/<\/?[^>]*>/, "")
+#end
+sentence = MeCab::Tagger.new()
+node = sentence.parseToNode("天気晴朗なれども波高し")
+while node do
+    str = node.feature.force_encoding("UTF-8")
+    if /^名詞/ =~ str
+	puts node.surface
+    end
+    node = node.next
 end
