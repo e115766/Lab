1709bc0cacb02acb9ce9e9da9da096af5420b950
Tasuku KINJO <e115766@ie.u-ryukyu.ac.jp>
2014-06-28 22:12:46 +0900

    ok

--- test.rb
+++ test.rb
@@ -1,15 +1,20 @@
 require 'mysql'
 require 'MeCab'
-#db = Mysql.connect('localhost', 'root', '', 'diseases')
-#db.query('select id, name, diagnose from diseases where diagnose like "%症状の現れ方%"').each do |col1, col2, col3|
-#    print col3.gsub(/<\/?[^>]*>/, "")
-#end
+data = []
+db = Mysql.connect('localhost', 'root', '', 'diseases')
 sentence = MeCab::Tagger.new()
-node = sentence.parseToNode("天気晴朗なれども波高し")
-while node do
-    str = node.feature.force_encoding("UTF-8")
-    if /^名詞/ =~ str
-	puts node.surface
+db.query('select id, name, description from diseases where description like "%症状の現れ方%"').each do |col1, col2, col3|
+    node = sentence.parseToNode(col3.gsub(/<\/?[^>]*>|[A-z]|[Ａ-ｚ]|[0-9]|[０-９]/, ""))
+    while node do
+	str = node.feature.force_encoding("UTF-8")
+	if /^動詞/ =~ str
+	    data.push(node.surface)
+	end
+	node = node.next
     end
-    node = node.next
 end
+
+k = Hash.new(0)
+data.each{|x| k[x] += 1}
+k = k.sort{|(k1, v1), (k2, v2)| v1 <=> v2 }
+k.each{|x| puts x[0], x[1]}
