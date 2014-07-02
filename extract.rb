206f98025e227fd0ce606bc6c3b64e4ab8e866e9
Tasuku KINJO <e115766@ie.u-ryukyu.ac.jp>
2014-07-02 12:35:52 +0900

    extratv05

--- extract.rb
+++ extract.rb
@@ -1,8 +1,17 @@
 require 'mysql'
 require 'MeCab'
+def extract(str)
+    /<h2 class=\"ttl\">症状の現れ方<\/h2>/ =~ str
+    /<\/p>/ =~ $'
+    p $`
+end
+
+
 data = []
 db = Mysql.connect('localhost', 'root', '', 'diseases')
 sentence = MeCab::Tagger.new()
 db.query('select id, name, description from diseases where description like "%症状の現れ方%"').each do |col1, col2, col3|
+    extract(col3)
 end
+
 
